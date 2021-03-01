1ec4f86c3e2056ba6cd0444b0d671651710eb82a
tessam30 <tim.essam@gmail.com>
2021-03-01 15:55:39 -0500

    add bar sparks for fy21q1

--- Scripts/FY21Q1_bar_sparks.R
+++ Scripts/FY21Q1_bar_sparks.R
@@ -29,12 +29,32 @@
     
   # Functions  
   merdata <- glamr::si_path("path_msd")
-  rasdata <- glamr::si_path("path_raster")
-  shpdata <- glamr::si_path("path_vector")
-  datim   <- glamr::si_path("path_datim")
+
 
   # Indicators
-  indic <- c("TX_NEW", "TX_CURR", "HTS_TST_POS" )
+  
+  # Plot to create tiny progress graphs in bar form
+  # Requires a long data frame named df_long
+  bar_spark <- function(df, ind, share) {
+    
+    viz <- df %>% 
+      filter(indicator == {{ind}}, fundingagency == "USAID") %>% 
+      ggplot() + 
+      geom_col(aes(fundingagency, y = 1), fill = trolley_grey_light) +
+      # old color #e04745
+      geom_col(aes(fundingagency, {{share}}), fill = denim) +
+      coord_flip() + 
+      theme_void() +
+      theme(legend.position = "none",
+            plot.background = element_rect(fill = "white",
+                                           color = "white"),
+      )
+    
+    ggsave(file.path(graphs, paste0("FY20Q1_sparks", {{ind}}, "_", deparse(substitute(share)), ".png")),
+            plot = viz, dpi = 330, width = 1.25, height = 0.1)
+    
+    print(viz)
+  } 
   
 # LOAD DATA ============================================================================  
   msd <- return_latest(folderpath = merdata, pattern = "OU_IM_FY19-21")
@@ -60,10 +80,13 @@
   
   
   # Calculate Agency shares and totals
-  df %>% filter(
-    standardizeddisaggregate == "Total Numerator",
-    indicator %in% c("TX_CURR", "TX_NEW", "HTS_TST_POS", "PrEP_NEW"),
-    fiscal_year == "2021") %>% 
+  df_long <- 
+    df %>% 
+    filter(
+      standardizeddisaggregate == "Total Numerator",
+      indicator %in% c("TX_CURR", "TX_NEW", "HTS_TST_POS", "PrEP_NEW"),
+      fiscal_year == "2021"
+    ) %>% 
     group_by(fundingagency, indicator) %>% 
     summarise(across(c(targets, cumulative), sum, na.rm = T)) %>%
     rbind(tx_mmd) %>% 
@@ -71,12 +94,23 @@
     group_by(indicator) %>% 
     mutate(across(c(targets, cumulative), sum, na.rm = T, .names = "all_{.col}")) %>%
     ungroup() %>% 
-    mutate(agency_share = cumulative / all_targets) %>% arrange(indicator, fundingagency) %>% 
-    prinf()
+    mutate(agency_share = cumulative / all_cumulative) %>% arrange(indicator, fundingagency) %>% 
+    filter(fundingagency == "USAID") %>% 
+    mutate(agency_share = if_else(indicator == "TX_MMD", 0.75, agency_share))
   
 # VIZ ============================================================================
 
-  #  
+  #  Sample plot
+  unique(df_long$indicator) %>% 
+    map(~bar_spark(df_long, ind = .x, agency_share))
+  
+  unique(df_long$indicator) %>% 
+    map(~bar_spark(df_long, ind = .x, achievement))
+  
+  
+  bar_spark(df_long, "HTS_TST_POS", agency_share)
 
 # SPINDOWN ============================================================================
 
+names(df_long)
+  
