5d21ba2fe62d6116dd4c2ecc21604401fc6fd89f
Aaron Chafetz <achafetz@usaid.gov>
2021-03-09 13:09:24 -0500

    dodge points

--- Scripts/FY21Q1_GLOBAL_UEs-Agency.R
+++ Scripts/FY21Q1_GLOBAL_UEs-Agency.R
@@ -28,15 +28,17 @@ df <- read_csv("Data/FY21Q1_UEs.csv")
 # MUNGE -------------------------------------------------------------------
 
 df <- df %>% 
-  mutate(agency = fct_inorder(agency))
+  mutate(agency = fct_rev(agency))
 
 # PLOT --------------------------------------------------------------------
+dodge <- position_dodge(width=.5)
 
 df %>% 
   ggplot(aes(ue, fct_reorder(category, ue), fill = agency)) +
-  geom_point(data = filter(df, agency == "CDC"), shape = 21, color = 'white', stroke = 1.4, size = 9) +
-  geom_point(data = filter(df, agency == "USAID"), shape = 21, color = 'white', stroke = 1.4, size = 9) +
+  geom_point(shape = 21, color = "white",stroke = 1.4, size = 9,
+             position = dodge) +
   # geom_text(aes(label = ue))
+  scale_y_reordered() +
   scale_fill_manual(values = c("USAID" = denim, "CDC" = trolley_grey_light)) +
   labs(x = "Unit Expenditure (FY20, $)", y = NULL,
        caption = "Note: Exclusives mechanisms that do not have associated targets and CDC NDOH mechanism 16772
